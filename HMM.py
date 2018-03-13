0ec696416803ebd16ebe5a385a3d5397ef6dcd25
Maksim Egorov <megorov@uh.edu>
2018-03-12 20:54:19 -0500

    Add files via upload

--- HMM.py
+++ HMM.py
@@ -8,14 +8,19 @@ resultList = []
 def counting_probabilities(file_path):  # returns unigram and bigram counts for probability matrix
     sentence_counter = 0
     previous_tag = None
+    previouse_word_language = None
     tag_count_dictionary = {}
     spa_tag_count={}
     eng_tag_count={}
     transition_count = {}
+    spa_transition_count={}
+    eng_transition_count={}
     emission_count = {}
     spanish_emission_count={}
     english_emission_count={}
     word_counting = {}
+    spa_word_counting ={}
+    eng_word_counting ={}
     for line in open(file_path,'r',encoding='UTF-8'):
         list_line = []
         for col in line.split():
@@ -25,25 +30,45 @@ def counting_probabilities(file_path):  # returns unigram and bigram counts for 
                 transition_count[tuple(['END', previous_tag])] += 1
             else:
                 transition_count[tuple(['END', previous_tag])] = 1
+            if previouse_word_language in ['spa','eng&spa','UNK']:
+                if tuple(['END', previous_tag]) in spa_transition_count:
+                    spa_transition_count[tuple(['END', previous_tag])]+=1
+                else:
+                    spa_transition_count[tuple(['END', previous_tag])]=1
+            if previouse_word_language in ['eng','eng&spa','UNK']:
+                if tuple(['END', previous_tag]) in eng_transition_count:
+                    eng_transition_count[tuple(['END', previous_tag])]+=1
+                else:
+                    eng_transition_count[tuple(['END', previous_tag])]=1
             previous_tag = None  # for computing start to tag
+            previouse_word_language=None
         else:
-            #for good turing smoothing
+            #for smoothing
             if list_line[0] in word_counting:
                 word_counting[list_line[0]]+=1
             else:
                 word_counting[list_line[0]]=1
-
+            if list_line[1] in ['spa','eng&spa','UNK']:
+                if list_line[0] in spa_word_counting:
+                    spa_word_counting[list_line[0]]+=1
+                else:
+                    spa_word_counting[list_line[0]]=1
+            if list_line[1] in ['eng','eng&spa','UNK']:
+                if list_line[0] in eng_word_counting:
+                    eng_word_counting[list_line[0]]+=1
+                else:
+                    eng_word_counting[list_line[0]]=1
 
             if list_line[2] in tag_count_dictionary:  # counting tag occurrences
                 tag_count_dictionary[list_line[2]] += 1  # counting delimiter for emission
             else:
                 tag_count_dictionary[list_line[2]] = 1
-            if list_line[1]=='spa':
+            if list_line[1] in ['spa','eng&spa','UNK']:
                 if list_line[2] in spa_tag_count:
                     spa_tag_count[list_line[2]]+=1
                 else:
                     spa_tag_count[list_line[2]]=1
-            elif list_line[1]=='eng':
+            if list_line[1] in ['eng','eng&spa','UNK']:
                 if list_line[2] in eng_tag_count:
                     eng_tag_count[list_line[2]]+=1
                 else:
@@ -54,26 +79,47 @@ def counting_probabilities(file_path):  # returns unigram and bigram counts for 
                     transition_count[tuple([list_line[2], 'START'])] += 1
                 else:  # creating new one
                     transition_count[tuple([list_line[2], 'START'])] = 1
+                if list_line[1] in ['spa','eng&spa','UNK']:
+                    if list_line[2] in spa_transition_count:
+                        spa_transition_count[tuple([list_line[2], 'START'])]+=1
+                    else:
+                        spa_transition_count[tuple([list_line[2], 'START'])]=1
+                if list_line[1] in ['eng','eng&spa','UNK']:
+                    if list_line[2] in eng_transition_count:
+                        eng_transition_count[tuple([list_line[2], 'START'])]+=1
+                    else:
+                        eng_transition_count[tuple([list_line[2], 'START'])]=1
             else:  # we are in the middle of sentence
                 if tuple([list_line[2], previous_tag]) in transition_count:
                     transition_count[tuple([list_line[2], previous_tag])] += 1
                 else:
                     transition_count[tuple([list_line[2], previous_tag])] = 1
+                if list_line[1] in ['spa','eng&spa','UNK']:
+                    if list_line[2] in spa_transition_count:
+                        spa_transition_count[tuple([list_line[2], previous_tag])]+=1
+                    else:
+                        spa_transition_count[tuple([list_line[2], previous_tag])]=1
+                if list_line[1] in ['eng','eng&spa','UNK']:
+                    if list_line[2] in eng_transition_count:
+                        eng_transition_count[tuple([list_line[2], previous_tag])]+=1
+                    else:
+                        eng_transition_count[tuple([list_line[2], previous_tag])]=1
             previous_tag = list_line[2]
+            previouse_word_language = list_line[1]
             if tuple([list_line[2], list_line[0]]) in emission_count:  # counting emission probability
                 emission_count[tuple([list_line[2], list_line[0]])] += 1
                 
             else:
                 emission_count[tuple([list_line[2], list_line[0]])] = 1
                 
-            if list_line[1] == 'spa' and tuple([list_line[2], list_line[0]]) in spanish_emission_count:
+            if list_line[1] in ['spa','eng&spa','UNK'] and tuple([list_line[2], list_line[0]]) in spanish_emission_count:
                     spanish_emission_count[tuple([list_line[2], list_line[0]])] += 1
-            elif list_line[1] == 'spa':
+            elif list_line[1] in ['spa','eng&spa','UNK']:
                     spanish_emission_count[tuple([list_line[2], list_line[0]])] = 1
                     
-            if list_line[1] == 'eng' and  tuple([list_line[2], list_line[0]]) in english_emission_count:
+            if list_line[1] in ['eng','eng&spa','UNK'] and  tuple([list_line[2], list_line[0]]) in english_emission_count:
                     english_emission_count[tuple([list_line[2], list_line[0]])] += 1
-            elif list_line[1] == 'eng':
+            elif list_line[1] in ['eng','eng&spa','UNK']:
                     english_emission_count[tuple([list_line[2], list_line[0]])] = 1
 
 
@@ -81,12 +127,24 @@ def counting_probabilities(file_path):  # returns unigram and bigram counts for 
         transition_count[tuple(['END', previous_tag])] += 1
     else:
         transition_count[tuple(['END', previous_tag])] = 1
+    #COUNtING PROBABILITY FOR UNSEEN WORDS WITH WORDS OCCURED ONLY ONCE
+    unseen_dictionary={}
+    for word,value in word_counting.items():
+        if value == 1:#this word occures only once so we need to add (tag,unseen_word) into dictionary
+            for key,v in emission_count.items():
+                if word in key:
+                    if tuple([key[0],'unseen_word']) in unseen_dictionary:
+                        unseen_dictionary[tuple([key[0],'unseen_word'])]+=1
+                    else:
+                        unseen_dictionary[tuple([key[0],'unseen_word'])]=1
+    emission_count.update(unseen_dictionary)
     return tag_count_dictionary,spa_tag_count,eng_tag_count, transition_count, emission_count, \
-    sentence_counter,word_counting,spanish_emission_count,english_emission_count
+    sentence_counter,word_counting,spanish_emission_count,english_emission_count,  spa_transition_count,\
+    eng_transition_count,eng_word_counting,spa_word_counting
 
 
 def viterbi(test_data_path, state_graph, tag_count,spa_tag_count,eng_tag_count, transition_count, emission_count,spanish_emission_count,english_emission_count,\
- sentence_counter,word_counting):
+ sentence_counter,word_counting,spa_transition_count, eng_transition_count,eng_word_counting,spa_word_counting):
     # reading data from test file line by line and compute needed viterbi values for each value
     sentences = []  # aka OBSERVATIONS
     language_in_sentence = []  # just easier to print that in the file
@@ -95,7 +153,6 @@ def viterbi(test_data_path, state_graph, tag_count,spa_tag_count,eng_tag_count, 
     good_turing_N=0
     for key,value in word_counting.items():
         good_turing_N +=value
-    good_turing_N_1 = good_turing_smoothing(word_counting,1)
     for line in open(test_data_path, 'r',encoding='UTF-8'):
         if len(line.split()) == 0:  # end of sentence so we can start our calculation for this sentence
             sentences.append(sentence)
@@ -118,30 +175,43 @@ def viterbi(test_data_path, state_graph, tag_count,spa_tag_count,eng_tag_count, 
         k = len(sentence)  # number of time steps
         viterbi_matrix = {}
         backpointer = {}
+        if lang[0] in ['spa','eng&spa']:
+            emission = spanish_emission_count
+            tag_variable = tag_count
+            transition_variable = transition_count
+            word_counting_variable = spa_word_counting
+        elif lang[0] in ['eng','eng&spa']:
+            emission = english_emission_count
+            tag_variable = tag_count
+            transition_variable = transition_count
+            word_counting_variable = eng_word_counting
+        else: 
+            emission = emission_count #all spa and eng words together
+            tag_variable = tag_count
+            transition_variable = transition_count
+            word_counting_variable = word_counting
         for state in state_graph:  # initialization step
             state_value = []
-            if tuple([state, 'START']) in transition_count:
-                a = transition_count[tuple([state, 'START'])] / sentence_counter
+            if tuple([state, 'START']) in transition_variable:
+                a = transition_variable[tuple([state, 'START'])] / sentence_counter
             else:
                 a=0
-            if lang[0] == 'spa':
-                emission = spanish_emission_count
-                tag_variable = spa_tag_count
-            elif lang[0]=='eng':
-                emission = english_emission_count
-                tag_variable = eng_tag_count
-            else: 
-                emission = emission_count #all spa and eng words together
-                tag_variable = tag_count
             if tuple([state, sentence[0]]) in emission:  # this word in dictionary
                 c = emission[tuple([state, sentence[0]])]
-                #b_new = ((c+1)*good_turing_smoothing(word_counting,c+1)/good_turing_smoothing(word_counting,c)) / tag_count[state]
                 b = c / tag_variable[state]
-            else:#unseen words handeled in this part  
+            elif (sentence[0] in word_counting_variable):#unseen words handeled in this part
+                b=0
+            else:  
                 if sentence[0][0].isupper() and state == 'PROPN':# if First letter of the word Upper case more likely it will be PROPN
-                    b =good_turing_N_1/good_turing_N
-                else:    
-                    b =good_turing_N_1/good_turing_N*0.00005
+                    b =0.5
+                else:
+                    if tuple([state,'unseen_word']) in emission_count:    
+                        b = emission_count[tuple([state,'unseen_word'])]/tag_count[state]
+                    else:
+                        if state in tag_count:
+                            b = 1/tag_count[state]
+                        else:
+                            b=0
             if sentence[0] == 'there' and state == 'PRON': #correction by hand
                 playing_with_dev_set = 1
             else:
@@ -151,46 +221,61 @@ def viterbi(test_data_path, state_graph, tag_count,spa_tag_count,eng_tag_count, 
             backpointer[state] = ['START']
         # recursion step
         for t in range(1, k):
+            aaa=0
+            if lang[t]  in ['spa'] and lang[t-1] == 'spa':
+                emission = spanish_emission_count
+                tag_variable = tag_count
+                transition_variable = transition_count
+                word_counting_variable = spa_word_counting
+            elif lang[t] in ['eng'] and lang[t-1] == 'eng':
+                emission = english_emission_count
+                tag_variable = tag_count
+                transition_variable = transition_count
+                word_counting_variable=eng_word_counting
+            else: 
+                emission = emission_count #all spa and eng words together
+                tag_variable = tag_count
+                transition_variable = transition_count
+                word_counting_variable = word_counting
             for state in state_graph:
                 links_value = []  # from this values we are going to choose maximum value
                 backtrack_values = []
                 for previouse_step_state in state_graph:  # computing new value based on previouse step and choosing
                     # maximum at the end
-                    if tuple([state, previouse_step_state]) in transition_count:  # not new transition between tags
-                        a = transition_count[tuple([state, previouse_step_state])] / tag_count[previouse_step_state]
+                    if tuple([state, previouse_step_state]) in transition_variable:  # not new transition between tags
+                        a = transition_variable[tuple([state, previouse_step_state])] / tag_variable[previouse_step_state]
                     else:
                         a=0
-                    if lang[t] == 'spa':
-                        emission = spanish_emission_count
-                        tag_variable = spa_tag_count
-                    elif lang[t]=='eng':
-                        emission = english_emission_count
-                        tag_variable = eng_tag_count
-                    else: 
-                        emission = emission_count #all spa and eng words together
-                        tag_variable = tag_count
                     if tuple([state, sentence[t]]) in emission:  # not new word to this tag
                         c = emission[tuple([state, sentence[t]])]
-                        #b_new = ((c+1)*good_turing_smoothing(word_counting,c+1)/good_turing_smoothing(word_counting,c)) / tag_count[state]
                         b = c / tag_variable[state]
-                    else: #unseen words handels here
+                    elif (sentence[t] in word_counting_variable): 
+                        b=0
+                    else:   #unseen words handels here
                         if sentence[t][0].isupper() and state == 'PROPN':# if First letter of the word Upper case more likely it will be PROPN
-                            b =good_turing_N_1/good_turing_N
+                            b =0.5
                         else:    
-                            b =good_turing_N_1/good_turing_N*0.00005
+                            if tuple([state,'unseen_word']) in emission_count:    
+                                b = emission_count[tuple([state,'unseen_word'])]/tag_count[state]
+                            else:
+                                if state in tag_count:
+                                    b = 1/tag_count[state]
+                                else:
+                                    b=0
                     playing_with_dev_set = 0
-                    if sentence[t] in ["'m","'s","'re","are","is", "am","was","were","have","do"] :
+
+                    if sentence[t] in ["'m","'s","'re","are","is", "am","was","were","have","do","did"] :
                         if len(sentence)-1 > t:
-                            if ((sentence[t]=="have" and sentence[t+1]=="to") or (sentence[t]=="do" and sentence[t+1]=="n't")) and state == 'AUX':
-                                playing_with_dev_set = 0.005
+                            if ((sentence[t]=="have" and sentence[t+1]=="to") or (sentence[t] in ["do","did"] and sentence[t+1]=="n't")) and state == 'AUX':
+                                playing_with_dev_set = 0.01
                             elif len(sentence[t+1])>3:
                                 if sentence[t+1][-3:]=="ing" and state == 'AUX':
-                                    playing_with_dev_set = 0.005
+                                    playing_with_dev_set = 0.01
                     if (sentence[t] == 'no' and state=='ADV') or \
                             (sentence[t] == 'voy' and state=='AUX') or \
                             (sentence[t] == 'about' and state == 'ADP'):
                            # (sentence[t]=='there' and (previouse_step_state in ['CCONJ','SCONJ']) and state == 'PRON'):
-                        playing_with_dev_set = 0.005
+                        playing_with_dev_set = 0.01
                     
                     links_value.append(viterbi_matrix[previouse_step_state][t - 1] * a * b+playing_with_dev_set)
                     backtrack_values.append(viterbi_matrix[previouse_step_state][t - 1] * a)
@@ -200,8 +285,9 @@ def viterbi(test_data_path, state_graph, tag_count,spa_tag_count,eng_tag_count, 
         links_value = []
         backtrack_values = []
         for state in state_graph:
-            if tuple(['END', state]) in transition_count:  # not new transition between tags
-                a = transition_count[tuple(['END', state])] / tag_count[state]
+            #tag_variable = tag_count
+            if tuple(['END', state]) in transition_variable:  # not new transition between tags
+                a = transition_variable[tuple(['END', state])] / tag_variable[state]
             else:
                 a = 0
             links_value.append(viterbi_matrix[state][len(sentence) - 1] * a)
@@ -254,7 +340,7 @@ def cmpFiles(experimentResultFile, trueResultFile,word_counting):
                 print('True = ',trueLine,'Exper = ',expLine,'Line = ',line_counter)
             line_counter+=1
     return round((total_number_results - negative_results) / total_number_results, 4)
-
+'''
 def good_turing_smoothing(emission_count,c):#retrund Nc value, c - word occurencies
     frequency =[]
     count=0
@@ -273,8 +359,9 @@ def good_turing_smoothing(emission_count,c):#retrund Nc value, c - word occurenc
         if count ==0:
             count = 1
         return count
-
-tag_count,spa_tag_count,eng_tag_count, transition, emission, sentance_count,word_counting,spanish_emission_count,english_emission_count = \
+'''
+tag_count,spa_tag_count,eng_tag_count, transition, emission, sentance_count,word_counting,spanish_emission_count,\
+english_emission_count,spa_transition_count, eng_transition_count,eng_word_counting,spa_word_counting = \
 counting_probabilities('dataset/train.conll')
 state_graph = ['ADJ', 'ADP', 'ADV', 'AUX', 'CCONJ', 'DET', 'INTJ', 'NOUN', 'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT',
                'SCONJ', 'SYM', 'VERB', 'X','UNK']
@@ -286,9 +373,9 @@ print(eng_tag_count['DET'])
 #print(tuple(['PART','to']),emission[tuple(['PART','to'])])
 #print(emission[tuple(['PART','to'])])
 viterbi('dataset/test.conll', state_graph, tag_count,spa_tag_count,eng_tag_count, transition,\
-emission,spanish_emission_count,english_emission_count, sentance_count,word_counting)
+emission,spanish_emission_count,english_emission_count, sentance_count,word_counting,spa_transition_count,eng_transition_count,eng_word_counting,spa_word_counting)
 #viterbi('dataset/small_dataset.txt', state_graph, tag_count,spa_tag_count,eng_tag_count, transition, emission,spanish_emission_count,english_emission_count,\
- #sentance_count,word_counting)
+#sentance_count,word_counting,spa_transition_count, eng_transition_count,eng_word_counting,spa_word_counting)
 '''
 orig_stdout = sys.stdout
 fout = open('wrongpredictions.txt', 'w')
